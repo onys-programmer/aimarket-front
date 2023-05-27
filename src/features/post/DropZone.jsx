b41ceacb143a88e1d0ca0b841114bd3b9b46912e
taehokim-onys <taeho.kim@timeblocks.com>
2023-05-28 02:58:04 +0900

    implement posting page

---
+++ src/features/post/DropZone.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import styled from '@emotion/styled';
+
+export default function DropZone(props) {
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps
+  } = useDropzone({
+    accept: {
+      'image/jpg': [],
+      'image/jpeg': [],
+      'image/png': []
+    }
+  });
+
+  const acceptedFileItems = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+      <ul>
+        {errors.map(e => (
+          <li key={e.code}>{e.message}</li>
+        ))}
+      </ul>
+    </li>
+  ));
+
+  return (
+    <S.Container>
+      <section className="container">
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+          <em>(Only *.jpg, *.jpeg and *.png images will be accepted)</em>
+        </div>
+        <aside>
+          <h4>Accepted files</h4>
+          <ul>{acceptedFileItems}</ul>
+          <h4>Rejected files</h4>
+          <ul>{fileRejectionItems}</ul>
+        </aside>
+      </section>
+    </S.Container>
+  );
+}
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    border: 1px solid #ddd;
+    background-color: #fafafa;
+    color: #7d7d7d;
+    border-radius: 16px;
+    padding: 30px;
+  `,
+};
--- src/features/post/PostingBox.jsx
+++ src/features/post/PostingBox.jsx
@@ -1,6 +1,7 @@
-import { Card, Stack, Input, Button, Textarea } from '@chakra-ui/react';
+import { Card, Flex, Stack, Input, Button, Textarea } from '@chakra-ui/react';
 import { useState } from 'react';
 import { BASE_URL } from '../../services/api/api';
+import DropZone from './DropZone';
 
 export default function PostingBox() {
   const token = localStorage.getItem('token');
@@ -47,13 +48,16 @@ export default function PostingBox() {
 
   return (
     <Card width='80vw' maxW={"1200px"} height='80vh' padding={'30px'} borderRadius={"24px"}>
-      <Stack height="100%">
-        <Input placeholder='제목' size="lg" onChange={handleChangeTitle} />
-        <Textarea placeholder='내용' height="100%" onChange={handleChangeContent} />
-        <Button colorScheme='red'>
-          저장
-        </Button>
-      </Stack>
+      <Flex height="100%" gap="36px">
+        <DropZone />
+        <Stack width="100%" height="100%">
+          <Input placeholder='제목' size="lg" onChange={handleChangeTitle} />
+          <Textarea placeholder='내용' height="100%" onChange={handleChangeContent} />
+          <Button colorScheme='red'>
+            저장
+          </Button>
+        </Stack>
+      </Flex>
     </Card>
   );
 };
