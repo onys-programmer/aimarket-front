d7cdb21e56eb58b0781b60e6998d50145f759368
onys-programmer <thkim2017@gmail.com>
2023-05-31 03:42:38 +0900

    edit BASE_URL

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://aimarket.herokuapp.com/api';
 
 const fetchPostByIndex = async (index) => {
   try {
